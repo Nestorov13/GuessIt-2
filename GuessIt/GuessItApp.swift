d61523b6d68e10bc961774651399354999b0cbd2
Nestorov13 <nes7orov@gmail.com>
2023-01-22 18:17:50 +0200

    Initial Commit

---
+++ GuessIt/GuessItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessItApp.swift
+//  GuessIt
+//
+//  Created by Borislav on 22.01.23.
+//
+
+import SwiftUI
+
+@main
+struct GuessItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
